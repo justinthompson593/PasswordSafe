c02b04b069a8e90e6eca17f6f4af52e4f6506d43
justinthompson593@gmail.com <justinthompson593@gmail.com>
2017-09-08 20:56:21 -0700

    this seems to work...haven't tested thoroughly

--- PasswordSafe/main.cpp
+++ PasswordSafe/main.cpp
@@ -16,6 +16,12 @@ void installPasswordSafe();
 void encryptPasswords();
 void decryptPasswords();
 
+string getGpgPassword(){
+	string out;
+	cout << "Enter your passphrase (the one I said to never EVER forget): ";
+	cin >> out;
+	return out;
+}
 
 
 int main(int argc, char* argv[]){
@@ -32,6 +38,7 @@ int main(int argc, char* argv[]){
 	
 	cout << "What would you like to do?" << endl;
 	cout << "1. Open safe to see passwords" << endl;
+	cout << "2. Add a password to safe" << endl;
 	
 	string usrIn;
 	cin >> usrIn;
@@ -39,11 +46,20 @@ int main(int argc, char* argv[]){
 	switch (stoi(usrIn)) {
   case 1:
 		{
-			sprintf(toSystem, "gpg $HOME/PasswordSafe/passwords.txt.gpg && echo \"$(<$HOME/PasswordSafe/passwords.txt)\" && rm -f $HOME/PasswordSafe/passwords.txt");
+//			sprintf(toSystem, "gpg $HOME/PasswordSafe/passwords.txt.gpg && echo \"$(<$HOME/PasswordSafe/passwords.txt)\" && rm -f $HOME/PasswordSafe/passwords.txt");
+			string pwd = getGpgPassword();
+			sprintf(toSystem, "gpg --decrypt --batch --passphrase \"%s\" $HOME/PasswordSafe/passwords.txt.gpg", pwd.c_str());
 			system(toSystem);
 		}
 			break;
-			
+  case 2:
+		{
+//			sprintf(toSystem, "gpg $HOME/PasswordSafe/passwords.txt.gpg && nano $HOME/PasswordSafe/passwords.txt && rm $HOME/PasswordSafe/passwords.txt");
+			string pwd = getGpgPassword();
+			sprintf(toSystem, "gpg --passphrase \"%s\" $HOME/PasswordSafe/passwords.txt.gpg && nano $HOME/PasswordSafe/passwords.txt && gpg -c --cipher-algo AES256 --passphrase \"%s\" $HOME/PasswordSafe/passwords.txt && rm $HOME/PasswordSafe/passwords.txt", pwd.c_str(), pwd.c_str());
+			system(toSystem);
+		}
+			break;
   default:
 			break;
 	}
@@ -75,8 +91,9 @@ void installPasswordSafe(){
 	}
 	
 	// make directory & build executable in it
-	sprintf(toSystem, "mkdir $HOME/PasswordSafe && cd $HOME/PasswordSafe && curl -o pwdSafe.cpp https://raw.githubusercontent.com/justinthompson593/PasswordSafe/master/PasswordSafe/main.cpp && g++ pwdSafe.cpp -std=c++11 -o PasswordSafe && rm -f pwdSafe.cpp && touch passwords.txt");
+	sprintf(toSystem, "mkdir $HOME/PasswordSafe && cd $HOME/PasswordSafe && curl -o pwdSafe.cpp https://raw.githubusercontent.com/justinthompson593/PasswordSafe/master/PasswordSafe/main.cpp && g++ pwdSafe.cpp -std=c++11 -o PasswordSafe && rm -f pwdSafe.cpp && echo \"______________Password Safe______________\" > passwords.txt && echo \"\" >> passwords.txt");
 	system(toSystem);
+	
 	
 	sprintf(toSystem, "echo \"Hit any key to add PasswordSafe to your path or c to cancel\n\"");
 	system(toSystem);
@@ -95,21 +112,32 @@ void installPasswordSafe(){
 		system(toSystem);
 #endif
 	}
+	
+	sprintf(toSystem, "echo \"Encrypting passwords.txt\nGet ready to choose a master passphrase for your safe. You will be asked to type it in twice. Do not forget it. If you do, the seas will boil, the sun will become black as sackcloth, and the moon as blood. Earthquakes. Famine. Plagues. Cats and dogs; living together. Mass hysteria.\n\nBut seriously, it is impossible to recover your passwords without this passphrase. Do not forget it. Hit any key to continue.\n\"");
+	system(toSystem);
+	sprintf(toSystem, "stty raw");
+	system(toSystem);
+	userIn = getchar();
+	sprintf(toSystem, "stty cooked");
+	system(toSystem);
+	
+	encryptPasswords();
+	
 }
 
 
 void decryptPasswords(){
-	char sysBuf[4096];
-	sprintf(sysBuf, "gpg passwords.txt.gpg");
+	char sysBuf[512];
+	sprintf(sysBuf, "gpg $HOME/PasswordSafe/passwords.txt.gpg");
 	system(sysBuf);
 }
 
 void encryptPasswords(){
-	char sysBuf[4096];
-	sprintf(sysBuf, "rm passwords.txt.gpg");
+	char sysBuf[512];
+	sprintf(sysBuf, "rm $HOME/PasswordSafe/passwords.txt.gpg 2> /dev/null");
 	system(sysBuf);
 	
-	sprintf(sysBuf, "gpg -c --cipher-algo AES256 passwords.txt && rm passwords.txt");
+	sprintf(sysBuf, "gpg -c --cipher-algo AES256 $HOME/PasswordSafe/passwords.txt && rm $HOME/PasswordSafe/passwords.txt");
 	system(sysBuf);
 }
 
